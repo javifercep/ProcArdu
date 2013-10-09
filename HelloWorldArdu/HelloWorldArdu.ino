795f5af03a3cef51fd57cd359e2c577b22cc67f9
Javier Fernández <j.fer.cep@gmail.com>
2013-10-09 14:58:36 +0200

    Update HelloWorldArdu.ino

--- HelloWorldArdu/HelloWorldArdu.ino
+++ HelloWorldArdu/HelloWorldArdu.ino
@@ -2,13 +2,17 @@ int i=0;
 
 void setup()
 {
+  /*Initializes the UART at 9600 bps*/
   Serial.begin(9600);
+  /*Sends first "Hello World!" message through the UART*/
   Serial.println("Hello World!");
+  /*Waits for a second*/
   delay(1000);
 }
 
 void loop()
 {
+  /*Sends more "Hellos World!", increases and prints the variable i every second*/
   Serial.print("Hello World! Again: ");
   Serial.println(++i);
   delay(1000);
